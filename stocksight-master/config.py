ce144e0f10c5b859f7c1db01b769e5f66ea2a781
Clark Minor <clarkminor@g.ucla.edu>
2020-03-23 18:29:10 -0700

    edit index.html

--- stocksight-master/config.py
+++ stocksight-master/config.py
@@ -6,11 +6,7 @@ consumer_key = "Pyi44bVudZ7ayXzNpDgwfygI9"
 consumer_secret = "AVy8tljg101CZ3zET3lADjC6tZlApV5CpC7bOednPfaxtvKDQl"
 access_token = "946257865185968128-W7KQm6gXU5VZ4SbiLpOtCaWCtBQXP2p"
 access_token_secret = "0kz2H9eHNLu9OflYOxnvGoC8D1OUfFC4h4c7FatT4OhaF"
-nltk_tokens_required = ('myl','mylan','manufacturing','hydroxychloroquine','hydroxychloraquine','sulfate','nasdaq','medication')#'covid-19','patient','fda','patients','pharmaceutical','nasdaq','covid19','who','cdc','medications') #('bicoin','btc','crypto','cryptocurrency','blockchain','digitalcurrency','satoshi','bitsahara','wallet','coin','ethereum','lightningcoin') #("KSS","Kohl's","Kohls","shop","clothes","deals","reward","#LoveKohls","clothing","handbags","cosmetics","beauty","department","shopping","leadership","sales","profit","shutdown","stores","retail","Sephora","ultabeauty") #("DDS","Dillard's","Shop","designer","dresses","shoes","clothing","handbags","cosmetics","beauty","bedding","wedding","registry","department","shopping","leadership","sales","profit","shutdown","stores","retail","close")
-#("NetApp", "hybrid cloud", "NTAP", "George Kurian", "DataDriven", "NetAppAssist")
-#("airlines", "delta", "delta airlines", "#delta", "travel","air travel", "airplaines", "dal", "DAL")
-#("clothing", "department", "store", "American Eagle", "AEO", "#americaneagle","fashion", "mall", "coronavirus")
-#TSLA ("neuralink", "solar", "tesla", "@tesla", "#tesla", "tesla", "tsla", "#tsla", "elonmusk", "elon", "musk", "spacex", "starlink")
+#nltk_tokens_required =
 nltk_min_tokens = 1
 nltk_tokens_ignored = ("win", "giveaway", "grocery", "fire", "police","senate","policy","pelosi","sen","democrat","democrats","republican","republicans","mayor","senator","congress","wuhan","china","love","eyes","government")
 twitter_feeds = ["@cnbc", "@benzinga", "@stockwits",
@@ -21,5 +17,5 @@ twitter_feeds = ["@cnbc", "@benzinga", "@stockwits",
                  "@muddywatersre", "@mcuban", "@AswathDamodaran", "@elerianm",
                  "@MorganStanley", "@ianbremmer", "@GoldmanSachs", "@Wu_Tang_Finance",
                  "@Schuldensuehner", "@NorthmanTrader", "@Frances_Coppola", "@bySamRo",
-                 "@BuzzFeed","@nytimes","@MylanNews"]
+                 "@BuzzFeed","@nytimes","@MylanNews","@BillGates","@Microsoft","@elonmusk","@coinbase","@Bitcoin","@blockchain","@BitcoinMagazine","@AEO","@Delta","@Kohls","@Dillards"]
 stocksight_token = "be8d6aa680cfe6a93ae2c6af6e876102ec729dea07c287abd228c2076ac8d64b2158def9321c16ed48d8c13dacfa488274e6"
--- stocksight-master/constant_data_collection.py
+++ stocksight-master/constant_data_collection.py
@@ -3,14 +3,21 @@ import time
 
 if __name__ == "__main__":
 
-    keywords = ['myl','mylan','manufacturing','hydroxychloroquine','hydroxychloraquine','sulfate','nasdaq','medication']
-    #("neuralink", "solar", "tesla", "@tesla", "#tesla", "tesla", "tsla", "#tsla", "elonmusk", "elon", "musk", "spacex", "starlink") #['myl','mylan','manufacturing','hydroxychloroquine','hydroxychloraquine','sulfate','nasdaq','medication']
-    #['bicoin','btc','crypto','cryptocurrency','blockchain','digitalcurrency','satoshi','bitsahara','wallet','coin','ethereum','lightningcoin']
-
-     #['s&p500','market','index','s&p','rising','falling','rise','fall','open','close','industries','stock','stocks','financial','nasdaq']
-    bashCommand = "python3 sentiment.py -s TSLA -k" + keywords[0] + ','.join(keywords[1:])
+    tickers = ['MYL','TSLA','BTC','^GSPC','KSS','DDS','NTAP','DAL','AEO']
+    keywords = [['myl','mylan','manufacturing','hydroxychloroquine','hydroxychloraquine','sulfate','nasdaq','medication'],
+    ["neuralink", "solar", "tesla", "@tesla", "#tesla", "tesla", "tsla", "#tsla", "elonmusk", "elon", "musk", "spacex", "starlink"],
+    ['bicoin','btc','crypto','cryptocurrency','blockchain','digitalcurrency','satoshi','bitsahara','digitalwallet','digitalcoin','ethereum','dogecoin'],
+    ['s&p500','index','s&p','bear','bull','rising','falling','marketopen','marketclose','industries','stock','stocks','financial','nasdaq','top500','stockmarket','djia','dowjones','industrialaverage'],
+    ["kss","kohl's","kohls","shop","clothes","deals","reward","#lovekohls","clothing","handbags","cosmetics","beauty","department","shopping","leadership","sales","profit","shutdown","stores","retail","sephora","ultabeauty"],
+    ["dds","dillards","shop","designer","dresses","shoes","clothing","handbags","cosmetics","beauty","bedding","wedding","registry","department","shopping","stores","retail"],
+    ["netapp", "#hybridcloud", "ntap", "georgekurian", "#datadriven", "netappassist"],
+    ["airlines", "delta", "deltaairlines", "#delta", "travel","airtravel", "airplaines", "dal"],
+    ["clothing", "department", "store", "americaneagle", "AEO", "#americaneagle","fashion", "mall","malls"]]
 
     while(True):
-        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-        output, error = process.communicate()
+        for i in range(len(tickers)):
+            bashCommand = "python3 sentiment.py -s" + tickers[i] + " -k" + keywords[i][0] + ','.join(keywords[i][1:]) + "--overridetokensreq" + keywords[i][0] + ','.join(keywords[i][1:]) + "--upload"
+            process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+            output, error = process.communicate()
+
         time.sleep(30)
